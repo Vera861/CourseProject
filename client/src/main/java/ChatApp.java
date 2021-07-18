a0d4f281b2f9342cddd97f4c6c29d7bd1379934f
Vera Meshkova <mverynchik@gmail.com>
2021-07-19 00:52:12 +0300

    interface

--- client/src/main/java/ChatApp.java
+++ client/src/main/java/ChatApp.java
@@ -1,15 +1,16 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 
 public class ChatApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent parent = FXMLLoader.load(getClass().getResource("chat.fxml"));
-        primaryStage.setScene(new Scene(parent));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("chat.fxml"));
+        primaryStage.setTitle("File Manager");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 }
--- client/src/main/java/ChatController.java
+++ client/src/main/java/ChatController.java
@@ -1,59 +1,121 @@
 import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
 import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
-import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import model.Message;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 
 public class ChatController implements Initializable {
+
     private final String root = "client/clientFiles";
     public TextField statusBar;
+    @FXML
+    private TableView<FileInfo> tableFile;
     private ObjectEncoderOutputStream os;
     private ObjectDecoderInputStream is;
     private byte[] buffer;
     private NettyNetwork network;
-    public ListView<String> listView;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        buffer = new byte[256];
-        try {
-            File dir = new File(root);
-            listView.getItems().clear();
-            listView.getItems().addAll(dir.list());
 
-            Socket socket = new Socket("localhost", 8189);
-            os = new ObjectEncoderOutputStream(socket.getOutputStream());
-            is = new ObjectDecoderInputStream(socket.getInputStream());
+        TableColumn<FileInfo, String> nameCol = new TableColumn<>("Name");
+        nameCol.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFilename()));
+        nameCol.setPrefWidth(240);
 
-            Thread readThread = new Thread(() -> {
-                try {
-                    while (true) {
-                        String status = is.readUTF();
-                        Platform.runLater(() -> statusBar.setText(status)
-                        );
+        TableColumn<FileInfo, Long> sizeCol = new TableColumn<>("Size");
+        sizeCol.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getSize()));
+        sizeCol.setCellFactory(column -> {
+            return new TableCell<FileInfo, Long>() {
+                @Override
+                protected void updateItem(Long item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty) {
+                        setText(null);
+                        setStyle("");
+                    } else {
+                        String text = String.format("%,d bytes", item);
+                        if (item == -1L) {
+                            text = "[DIR]";
+                        }
+                        setText(text);
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
-            });
-            readThread.setDaemon(true);
-            readThread.start();
-        } catch (Exception e) {
-            e.printStackTrace();
+            };
+        });
+        sizeCol.setPrefWidth(120);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        TableColumn<FileInfo, String> dateCol = new TableColumn<>("Date");
+        dateCol.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getLastModified().format(dtf)));
+        dateCol.setPrefWidth(120);
+
+        tableFile.getColumns().addAll(nameCol, sizeCol, dateCol);
+        tableFile.getSortOrder().add(nameCol);
+        updateList(Paths.get(root));
+    }
+
+    public void updateList(Path path) {
+        try {
+            tableFile.getItems().clear();
+            tableFile.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
+            tableFile.sort();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Failed to load file list", ButtonType.OK);
+            alert.showAndWait();
         }
     }
 
-    public void send(javafx.event.ActionEvent actionEvent) throws IOException {
+//
+//
+//
+////        buffer = new byte[256];
+////        try {
+//////            File dir = new File(root);
+//////            listView.getItems().clear();
+//////            listView.getItems().addAll(dir.list());
+////
+////            Socket socket = new Socket("localhost", 8189);
+////            os = new ObjectEncoderOutputStream(socket.getOutputStream());
+////            is = new ObjectDecoderInputStream(socket.getInputStream());
+////
+////            Thread readThread = new Thread(() -> {
+////                try {
+////                    while (true) {
+////                        String status = is.readUTF();
+////                        Platform.runLater(() -> statusBar.setText(status)
+////                        );
+////                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//            readThread.setDaemon(true);
+//            readThread.start();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+//    public void send(java.awt.event.ActionEvent actionEvent) throws IOException {
+//        String content = statusBar.getText();
+//        network.writeMessage(new Message(content));
+//    }
+
+    public void send(javafx.event.ActionEvent actionEvent) {
         String content = statusBar.getText();
         network.writeMessage(new Message(content));
     }
